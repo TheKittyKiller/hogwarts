c322adf68947e4a498fc85b449644a2b0bf627bd
sebastian melph <jame1307@stud.kea.dk>
2023-03-07 12:31:38 +0100

    first step

--- js.js
+++ js.js
@@ -1,32 +1,68 @@
 "use strict"
 
 
+let allStudnets = [];
 
-
-const Url = "https://petlatkea.dk/2021/hogwarts/students.json";
+const url = "https://petlatkea.dk/2021/hogwarts/students.json";
 const listElement = document.getElementById("student-list");
 
-let firstName;
-let middleName;
-let lastName;
-let houseName;
-let genderOf;
+
+const Student = {
+firstname: "",
+middlename: null,
+nickName: null,
+lastname: null,
+gender:"",
+house:"",
+}
+
+window.addEventListener("DOMContentLoaded", loadJSON);
 
 
 
-fetch(Url)
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(student => {
-      const listItem = document.createElement("li");
-      listItem.textContent = student.fullname + " " + student.house + " " + student.gender;
-      listElement.appendChild(listItem);
-    });
-  })
-  .catch(error => console.log("ther was an error cathing the data fam:", error));
+async function loadJSON() {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+
+    // when loaded, prepare data objects
+    prepareObjects(jsonData);
+}
 
 
 
+
+function prepareObjects(jsonData) {
+    allStudnets = jsonData.map(prepareObject);
+
+
+
+    console.table(allStudnets);
+
+    // displayList();
+}
+
+
+ 
+function prepareObject(jsonObject) {
+    const student = Object.create(Student);
+
+    student.firstname = jsonObject.fullname;
+
+    // const texts = jsonObject.fullname.split(" ");
+    // animal.name = texts[0];
+    // animal.desc = texts[2];
+    // animal.type = texts[3];
+    // animal.age = jsonObject.age;
+
+    return student;
+}
+
+
+function trimName(){
+
+student.firstname = jsonObject.trim; 
+
+}
 
 
 
