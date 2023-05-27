6fe01c88fc33765fee24d59ae9f5231753e5a9a4
TheKittyKiller <jame1307@stud.kea.dk>
2023-05-27 12:28:45 +0200

    sorting stdunets

--- js.js
+++ js.js
@@ -1,101 +1,148 @@
-"use strict"
+// "use strict";
+
+// let allStudents = [];
+
+// const url = "https://petlatkea.dk/2021/hogwarts/students.json";
+// const listElement = document.getElementById("student-list");
+
+// const Student = {
+//   firstname: "",
+//   middlename: "",
+//   nickName: "",
+//   lastname: "",
+//   gender: "",
+//   house: "",
+//   bloodType: "",
+// };
+
+// window.addEventListener("DOMContentLoaded", loadJSON);
+
+// async function loadJSON() {
+//   const response = await fetch(url);
+//   const jsonData = await response.json();
+
+//   prepareObjects(jsonData);
+//   displayList();
+// }
+
+// function prepareObjects(jsonData) {
+//   allStudents = jsonData.map(prepareObject);
+// }
+
+// function prepareObject(jsonObject) {
+//   const student = Object.create(Student);
+
+//   const fullname = jsonObject.fullname.trimStart().toLowerCase();
+//   const texts = fullname.split(" ");
+
+//   student.firstname = texts[0];
+//   student.middlename = texts[1];
+//   student.lastname = texts[2];
+//   student.gender = jsonObject.gender;
+//   student.house = jsonObject.house;
+
+//   return student;
+// }
+
+// function displayList() {
+//   listElement.innerHTML = "";
+
+//   allStudents.forEach((student) => {
+//     const listItem = document.createElement("li");
+//     listItem.textContent = `${student.firstname}     ${student.middlename} - ${student.house} - ${student.gender}    `;
+//     listElement.appendChild(listItem);
+//   });
+// }
 
 
-let allStudnets = [];
+"use strict";
 
 const url = "https://petlatkea.dk/2021/hogwarts/students.json";
-const listElement = document.getElementById("student-list");
-
 
 const Student = {
-firstname: "",
-middlename: null,
-nickName: null,
-lastname: null,
-gender:"",
-house:"",
-bloodType:""
+  firstname: "",
+  middlename: "",
+  lastname: "",
+  nickname: "",
+  house: "",
+};
 
-}
+let cleanedData = [];
 
-
-
-window.addEventListener("DOMContentLoaded", loadJSON);
-
-
-async function loadJSON() {
+async function cleanData() {
+  try {
     const response = await fetch(url);
     const jsonData = await response.json();
 
-    // when loaded, prepare data objects
-    prepareObjects(jsonData);
+    cleanedData = jsonData.map((studentData) => {
+      const student = Object.create(Student);
+
+      const fullname = studentData.fullname.trim();
+      const names = fullname.split(" ");
+
+      student.firstname = capitalizeFirstLetter(names.shift());
+      student.lastname = capitalizeFirstLetter(names.pop());
+
+      if (names.length > 0) {
+        const potentialNickname = names.join(" ").replace(/"/g, "");
+        if (!potentialNickname.includes("(") && !potentialNickname.includes(")")) {
+          student.middlename = capitalizeFirstLetter(names.join(" "));
+        } else {
+          student.nickname = potentialNickname
+            .replace("(", "")
+            .replace(")", "")
+            .toLowerCase();
+        }
+      }
+
+      student.house = studentData.house.trim();
+
+      return student;
+    });
+
+    displayData(cleanedData);
+  } catch (error) {
+    console.error("An error occurred while cleaning the data:", error);
+  }
 }
 
-
-
-
-
-
-function prepareObjects(jsonData) {
-    allStudnets = jsonData.map(prepareObject);
-
-    // console.table(allStudnets);
-
-    // displayList();
+function capitalizeFirstLetter(string) {
+  if (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+  return "";
 }
 
+function displayData(students) {
+  const listElement = document.getElementById("student-list");
+  listElement.innerHTML = ""; // Clear previous data
 
- 
-function prepareObject(jsonObject) {
-    const student = Object.create(Student);
+  students.forEach((student) => {
+    const listItem = document.createElement("li");
 
-    const fullname = jsonObject.fullname.trimStart().toLowerCase();
+    let fullName = `${student.firstname} ${student.middlename} ${student.lastname}`;
+    if (student.nickname) {
+      fullName += ` (${student.nickname})`;
+    }
 
-
-    const firstSpace = fullname.indexOf(" ");
-    const secondSpace = fullname.indexOf(" ", firstSpace +1);
-    const lastSpace = fullname.lastIndexOf(" ");
-    
-    
-    const firstname = fullname.substring(0, firstSpace);
-    const middlename = fullname.substring(firstSpace, secondSpace);
-    const lastname = fullname.substring(lastSpace);
-    
-
-
-    console.log(fullname)
-
-
-    // const texts = jsonObject.fullname.split(" ");
-
-    // student.firstname = texts[0];
-    // student.middlename = texts[1];
-    // student.lastname = texts[2];
-
-   // for the console table 
-   // student.fullname =jsonObject.fullname;
-   //     student.gender = jsonObject.gender;
-   //     student.house = jsonObject.house;
-
-   // animal.age = jsonObject.age;
-
-    // trimName();
-    return student;
+    listItem.textContent = fullName;
+    listElement.appendChild(listItem);
+  });
 }
 
-
-
-
-function lowerCase (){
-
-
-
-
-    Student.fullname = jsonObject.toLowerCase();
+function sortDataByFirstName() {
+  cleanedData.sort((a, b) => a.firstname.localeCompare(b.firstname));
+  displayData(cleanedData);
 }
 
+function sortDataByLastName() {
+  cleanedData.sort((a, b) => a.lastname.localeCompare(b.lastname));
+  displayData(cleanedData);
+}
 
+function sortDataByHouse() {
+  cleanedData.sort((a, b) => a.house.localeCompare(b.house));
+  displayData(cleanedData);
+}
 
-
-
-
+cleanData();
