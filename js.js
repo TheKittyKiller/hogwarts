4abf76b20cd8fa5791daade3ba3bb76c27cd72a5
TheKittyKiller <jame1307@stud.kea.dk>
2023-05-27 12:44:16 +0200

    table system and cleaing house data

--- js.js
+++ js.js
@@ -70,43 +70,43 @@ const Student = {
 let cleanedData = [];
 
 
-
 async function cleanData() {
-  try {
-    const response = await fetch(url);
-    const jsonData = await response.json();
-
-    cleanedData = jsonData.map((studentData) => {
-      const student = Object.create(Student);
-
-      const fullname = studentData.fullname.trim();
-      const names = fullname.split(" ");
-
-      student.firstname = capitalizeFirstLetter(names.shift());
-      student.lastname = capitalizeFirstLetter(names.pop());
-
-      if (names.length > 0) {
-        const potentialNickname = names.join(" ").replace(/"/g, "");
-        if (!potentialNickname.includes("(") && !potentialNickname.includes(")")) {
-          student.middlename = capitalizeFirstLetter(names.join(" "));
-        } else {
-          student.nickname = potentialNickname
-            .replace("(", "")
-            .replace(")", "")
-            .toLowerCase();
+    try {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+  
+      cleanedData = jsonData.map((studentData) => {
+        const student = Object.create(Student);
+  
+        const fullname = studentData.fullname.trim();
+        const names = fullname.split(" ");
+  
+        student.firstname = capitalizeFirstLetter(names.shift());
+        student.lastname = capitalizeFirstLetter(names.pop());
+  
+        if (names.length > 0) {
+          const potentialNickname = names.join(" ").replace(/"/g, "");
+          if (!potentialNickname.includes("(") && !potentialNickname.includes(")")) {
+            student.middlename = capitalizeFirstLetter(names.join(" "));
+          } else {
+            student.nickname = potentialNickname
+              .replace("(", "")
+              .replace(")", "")
+              .toLowerCase();
+          }
         }
-      }
-
-      student.house = studentData.house.trim();
-
-      return student;
-    });
-
-    displayData(cleanedData);
-  } catch (error) {
-    console.error("An error occurred while cleaning the data:", error);
+  
+        student.house = capitalizeFirstLetter(studentData.house.trim());
+  
+        return student;
+      });
+  
+      displayData(cleanedData);
+    } catch (error) {
+      console.error("An error occurred while cleaning the data:", error);
+    }
   }
-}
+  
 
 
 
@@ -121,26 +121,42 @@ function capitalizeFirstLetter(string) {
 
 //putting the list on html
 function displayData(students) {
-    const listElement = document.getElementById("student-list");
-    listElement.innerHTML = ""; // Clear previous data
+    const tableElement = document.getElementById("student-table");
+    tableElement.innerHTML = ""; // Clear previous data
   
+    const tableHeader = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+    headerRow.innerHTML = `<th>First Name</th><th>Middle Name</th><th>Last Name</th><th>Nickname</th><th>House</th>`;
+    tableHeader.appendChild(headerRow);
+    tableElement.appendChild(tableHeader);
+  
+    const tableBody = document.createElement("tbody");
     students.forEach((student) => {
-      const listItem = document.createElement("li");
+      const row = document.createElement("tr");
+      const firstNameCell = document.createElement("td");
+      const middleNameCell = document.createElement("td");
+      const lastNameCell = document.createElement("td");
+      const nicknameCell = document.createElement("td");
+      const houseCell = document.createElement("td");
   
-      let fullName = `${student.firstname} ${student.middlename} ${student.lastname}`;
-      if (student.nickname) {
-        fullName += ` (${student.nickname})`;
-      }
+      firstNameCell.textContent = student.firstname;
+      middleNameCell.textContent = student.middlename;
+      lastNameCell.textContent = student.lastname;
+      nicknameCell.textContent = student.nickname;
+      houseCell.textContent = student.house;
   
-      const house = document.createElement("span");
-      house.textContent = ` House: ${student.house}`;
-      house.classList.add("house"); // Add a class for styling if desired
+      row.appendChild(firstNameCell);
+      row.appendChild(middleNameCell);
+      row.appendChild(lastNameCell);
+      row.appendChild(nicknameCell);
+      row.appendChild(houseCell);
   
-      listItem.textContent = fullName;
-      listItem.appendChild(house);
-      listElement.appendChild(listItem);
+      tableBody.appendChild(row);
     });
+  
+    tableElement.appendChild(tableBody);
   }
+  
   
 
 
