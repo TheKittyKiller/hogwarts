191f03a4ef1266eb504e6a75d496c0ca41bf2307
TheKittyKiller <jame1307@stud.kea.dk>
2023-05-29 14:31:14 +0200

    details pop up

--- js.js
+++ js.js
@@ -3,11 +3,13 @@
 const url = "https://petlatkea.dk/2021/hogwarts/students.json";
 
 const Student = {
-    firstname: "",
-    lastname: "",
-    house: "",
-    nicknames: [] // Add nicknames property
-  };
+  firstname: "",
+  middlename: "", // New property
+  lastname: "",
+  house: "",
+  nicknames: []
+};
+
   
 let allStudents = [];
 let filteredStudents = [];
@@ -21,20 +23,23 @@ async function fetchData() {
     const response = await fetch(url);
     const jsonData = await response.json();
     allStudents = jsonData.map((studentData) => {
-        const student = Object.create(Student);
-        const fullNameParts = studentData.fullname.trim().split(' ');
-        const firstName = capitalizeFirstLetter(fullNameParts[0]);
-        const lastName = fullNameParts.length > 1 ? capitalizeFirstLetter(fullNameParts[fullNameParts.length - 1].replace(/["']/g, '')) : '';
-        const house = capitalizeFirstLetter(studentData.house.trim());
-        student.firstname = firstName;
-        student.lastname = lastName;
-        student.house = house;
-        student.nicknames = findNicknames(studentData.fullname);
-        return student;
-      });
-      
-         
-    filteredStudents = [...allStudents]; // Initially, set filtered students as all students
+      const student = Object.create(Student);
+      const fullNameParts = studentData.fullname.trim().split(' ');
+
+      const firstName = capitalizeFirstLetter(fullNameParts[0]);
+      const middleName = capitalizeFirstLetter(fullNameParts.slice(1, -1).join(' '));
+      const lastName = fullNameParts.length > 1 ? capitalizeFirstLetter(fullNameParts[fullNameParts.length - 1].replace(/["']/g, '')) : '';
+
+      const house = capitalizeFirstLetter(studentData.house.trim());
+      student.firstname = firstName;
+      student.middlename = middleName; // Assign middle name
+      student.lastname = lastName;
+      student.house = house;
+      student.nicknames = findNicknames(studentData.fullname);
+      return student;
+    });
+
+    filteredStudents = [...allStudents];
     displayData(filteredStudents);
   } catch (error) {
     console.error("An error occurred while fetching the data:", error);
@@ -111,15 +116,22 @@ function resetFilterAndSort() {
   displayData(filteredStudents);
 }
 
+
+
+
 function displayData(students) {
   const tableElement = document.getElementById("student-table");
   tableElement.innerHTML = ""; // Clear previous data
+
+
 
   const tableHeader = document.createElement("thead");
   const headerRow = document.createElement("tr");
   headerRow.innerHTML = `<th onclick="sortData('firstname')">First Name</th><th onclick="sortData('lastname')">Last Name</th><th onclick="sortData('house')">House</th><th onclick="sortData('nicknames')">Nicknames</th><th>Image</th>`; // Add new column header for image
   tableHeader.appendChild(headerRow);
   tableElement.appendChild(tableHeader);
+
+
 
   const tableBody = document.createElement("tbody");
   students.forEach((student) => {
@@ -135,6 +147,10 @@ function displayData(students) {
     houseCell.textContent = student.house;
     nicknamesCell.textContent = student.nicknames.join(", ");
 
+
+
+
+  
     // Create and append the image element
     const image = document.createElement("img");
     let imageName;
@@ -158,6 +174,8 @@ function displayData(students) {
     row.appendChild(nicknamesCell);
     row.appendChild(imageCell); // Add image cell to the row
 
+//click event listner
+  row.addEventListener("click", () => showStudentDetails(student));
     tableBody.appendChild(row);
   });
 
@@ -172,6 +190,64 @@ function displayData(students) {
   houseCountElement.textContent = `Number of students in each house: ${formatHouseCounts(houseCounts)}`;
   displayedCountElement.textContent = `Number of students currently displayed: ${displayedCount}`;
 }
+
+
+function showStudentDetails(student) {
+  // Create a pop-up container
+  const popupContainer = document.createElement("div");
+  popupContainer.classList.add("popup-container");
+
+  // Create the pop-up content
+  const popupContent = document.createElement("div");
+  popupContent.classList.add("popup-content");
+
+  // Create the student image element
+  const image = document.createElement("img");
+  let imageName;
+  if (filteredStudents.filter((s) => s.lastname === student.lastname).length > 1) {
+    imageName = `${student.lastname.toLowerCase()}_${student.firstname.toLowerCase()}.png`;
+  } else {
+    const lastNameParts = student.lastname.toLowerCase().split("-");
+    if (lastNameParts.length > 1) {
+      imageName = `${lastNameParts[lastNameParts.length - 1]}_${student.firstname.toLowerCase().charAt(0)}.png`;
+    } else {
+      imageName = `${student.lastname.toLowerCase()}_${student.firstname.toLowerCase().charAt(0)}.png`;
+    }
+  }
+  image.src = `images/${imageName}`;
+  image.alt = `${student.firstname} ${student.lastname}`;
+  popupContent.appendChild(image);
+
+  // Create the student name element
+  const name = document.createElement("h2");
+  name.textContent = `${student.firstname} ${student.middlename} ${student.lastname}`; // Include middle name
+  popupContent.appendChild(name);
+
+  // Create the student house element
+  const house = document.createElement("p");
+  house.textContent = `House: ${student.house}`;
+  popupContent.appendChild(house);
+
+  // Create the student nicknames element
+  const nicknames = document.createElement("p");
+  nicknames.textContent = `Nicknames: ${student.nicknames.join(", ")}`;
+  popupContent.appendChild(nicknames);
+
+  // Append the pop-up content to the container
+  popupContainer.appendChild(popupContent);
+
+  // Append the pop-up container to the body
+  document.body.appendChild(popupContainer);
+
+  // Close the pop-up when clicking outside of it
+  popupContainer.addEventListener("click", (event) => {
+    if (event.target === popupContainer) {
+      document.body.removeChild(popupContainer);
+    }
+  });
+}
+
+
 
 
 
